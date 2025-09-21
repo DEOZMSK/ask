3c5ccd4bf3fc5b4b1285addd50454e0f2218348c
DEOZMSK <jhfc47b67x3782@gmail.com>
2025-09-21 07:13:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const thanks = document.getElementById("thanks");
 
 // Вставь сюда свой URL из Google Apps Script (обязательно /exec)
 const SHEET_URL = "https://script.google.com/macros/s/AKfycbzvFbbFe5H9SwhD_1F_r5JIoAgSFPaQFwiqjsPTgmaHjrMmug83i2inqQ8CKpqcJDsz4g/exec";
+const intro = document.getElementById("screen-intro");
+const survey = document.getElementById("screen-survey");
+const btnStart = document.getElementById("btn-start");
+
+btnStart.addEventListener("click", () => {
+  intro.classList.add("hidden");     // спрятать экран «intro»
+  survey.classList.remove("hidden"); // показать экран с опросом
+  startSurvey();                     // функция, которая рендерит первый вопрос
+});
 
 form.addEventListener("submit", async (e)=>{
   e.preventDefault();
