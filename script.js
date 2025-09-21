ac5bd836587963b2bfe32a47570b7e8dcda3860f
DEOZMSK <jhfc47b67x3782@gmail.com>
2025-09-21 06:49:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const form = document.getElementById("survey-form");
 const thanks = document.getElementById("thanks");
 
-// Вставь сюда свой URL из Google Apps Script (обязательно /exec)
-const SHEET_URL = "https://script.google.com/macros/s/AKfycbzvFbbFe5H9SwhD_1F_r5JIoAgSFPaQFwiqjsPTgmaHjrMmug83i2inqQ8CKpqcJDsz4g/exec";
+// Вставь сюда свой URL из Google Apps Script
+const SHEET_URL = "https://script.google.com/macros/s/ВАШ_ID/exec";
 
 form.addEventListener("submit", async (e)=>{
   e.preventDefault();
   const data = Object.fromEntries(new FormData(form).entries());
 
-  const params = new URLSearchParams();
-  for (const [k,v] of Object.entries(data)) {
-    params.append(k,v);
-  }
-
-  try {
-    await fetch(SHEET_URL, {method:"POST", body:params});
+  try{
+    let res = await fetch(SHEET_URL, {
+      method:"POST",
+      body: JSON.stringify(data),
+      headers:{"Content-Type":"application/json"}
+    });
+    let json = await res.json();
+    console.log("Ответ сервера:", json);
     form.classList.add("hidden");
     thanks.classList.remove("hidden");
-  } catch(err) {
+  }catch(err){
     alert("Ошибка отправки: "+err);
   }
 });
